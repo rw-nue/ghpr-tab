41b3ceeba061c707aea46c3e21377854b276fe20
rwnue <wakabayashi@novatrade.co.jp>
2015-12-21 08:05:23 +0900

    code formatting

--- js/popup.js
+++ js/popup.js
@@ -3,18 +3,18 @@ $(function(){
   initInputValues();
   initInputListeners();
 
-	$('#go').on('click', function(){
-		var from = $('#from').val(); 
-		var to = $('#to').val(); 
-		var ghe = $('#url').val();
-		var compare = '/compare/' + to + '...' + from
-		var repos = $('#repos').val().split("\n");
-		for(var i = 0; i < repos.length; i++){
-			repo = repos[i];
-			url = ghe + repo + compare
-			chrome.tabs.create({url: url})
-		}
-	});
+  $('#go').on('click', function(){
+    var from = $('#from').val();
+    var to = $('#to').val();
+    var ghe = $('#url').val();
+    var compare = '/compare/' + to + '...' + from
+    var repos = $('#repos').val().split("\n");
+    for(var i = 0; i < repos.length; i++){
+      repo = repos[i];
+      url = ghe + repo + compare
+      chrome.tabs.create({url: url})
+    }
+  });
   function initInputValues(){
     $('#from').val(localStorage.from);
     $('#to').val(localStorage.to);
