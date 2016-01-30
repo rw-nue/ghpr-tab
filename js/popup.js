923a040b95344bf849796bc8f9574438a9da9fcb
rw-nue <wakabayashi@novatrade.co.jp>
2016-01-31 01:37:49 +0900

    refs #4 code format

--- js/popup.js
+++ js/popup.js
@@ -3,30 +3,30 @@ $(function(){
     initInputListeners();
     initSubmitButtonListeners();
 });
-  function initInputValues(){
-    $('#from').val(localStorage.from);
-    $('#to').val(localStorage.to);
-    $('#url').val(localStorage.url);
-    $('#repos').val(localStorage.repos);
-  }
-  function initInputListeners(){
-    $("#to").on('input', function(){
-      localStorage.to = $(this).val();
-    });
-    $("#from").on('input', function(){
-      localStorage.from = $(this).val();
-    });
-    $("#url").on('input', function(){
-      localStorage.url = $(this).val();
-    });
-    $("#repos").on('input', function(){
-      localStorage.repos = $(this).val();
-    });
-  }
-  function initSubmitButtonListeners(){
-    $('#go').on('click', function(){
-      var to = $('#to').val();
-      var from = $('#from').val();
-      openPrTabs(to, from);
-    });
-  }
+function initInputValues(){
+  $('#from').val(localStorage.from);
+  $('#to').val(localStorage.to);
+  $('#url').val(localStorage.url);
+  $('#repos').val(localStorage.repos);
+}
+function initInputListeners(){
+  $("#to").on('input', function(){
+    localStorage.to = $(this).val();
+  });
+  $("#from").on('input', function(){
+    localStorage.from = $(this).val();
+  });
+  $("#url").on('input', function(){
+    localStorage.url = $(this).val();
+  });
+  $("#repos").on('input', function(){
+    localStorage.repos = $(this).val();
+  });
+}
+function initSubmitButtonListeners(){
+  $('#go').on('click', function(){
+    var to = $('#to').val();
+    var from = $('#from').val();
+    openPrTabs(to, from);
+  });
+}
